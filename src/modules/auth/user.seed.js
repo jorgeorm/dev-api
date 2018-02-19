71fe1923ecc3df3d5ebc1c82ae2218b27eb3058c
Jorge Ordoñez Mendez <jorgeorm@gmail.com>
2018-02-19 09:51:54 -0500

    E0004.US0001: Changing Seeder
    Using env var for main admin user password

--- src/modules/auth/user.seed.js
+++ src/modules/auth/user.seed.js
@@ -2,9 +2,11 @@ const ADMIN = require('./roles.constants').ADMIN;
 const strHelpers = require('../../lib/string.helpers');
 const User = require('./user.model');
 
+const adminPass = process.env.ADMIN_PASSWORD || strHelpers.randomStr(5);
+
 const BASE_ADMIN = {
     email: 'jorgeorm@gmail.com',
-    password: strHelpers.randomStr(5),
+    password: adminPass,
     role: ADMIN
 };
 
